67579a4bf4a7730043f7eb852254bb6fe1b27073
Cleyton <cleyton.prudencio@grupoimg.com.br>
2025-05-15 21:15:20 -0300

    Subida basica do projeto.

---
+++ backend/src/test/java/br/com/rotaflex/RotaflexApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.rotaflex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RotaflexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
